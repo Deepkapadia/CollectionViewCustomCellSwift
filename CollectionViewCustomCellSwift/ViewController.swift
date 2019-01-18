cab41098532e0a7f8fc457cd31f95398597b7ca9
Deep_Kapadia <kapadia.deep12@gmail.com>
2019-01-18 10:59:47 +0530

    collectionView_CustomCell_Swift
    CollectionView with CustomCell file Swift Sample

--- CollectionViewCustomCellSwift/ViewController.swift
+++ CollectionViewCustomCellSwift/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    var arr:[String]? = nil;
+    var img:[String]? = nil;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        arr = ["iPhone","Asp.Net","Android","Java","PHP"];
+        img = ["image-1.jpg","image-1.jpg","image-1.jpg","image-1.jpg","image-1.jpg"];
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +26,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return (arr?.count)!;
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let c1=collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! firstcell;
+        
+        c1.imgv.image=UIImage(named: (img?[indexPath.row])!);
+        c1.lbl.text=arr?[indexPath.row];
+        
+        return c1;
+    }
 
 }
 
---
+++ firstcell.swift
@@ -0,0 +1,16 @@
+//
+//  firstcell.swift
+//  CollectionViewCustomCellSwift
+//
+//  Created by MAC OS on 6/1/17.
+//  Copyright © 2017 MAC OS. All rights reserved.
+//
+
+import UIKit
+
+class firstcell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgv: UIImageView!
+    
+    @IBOutlet weak var lbl: UILabel!
+}
